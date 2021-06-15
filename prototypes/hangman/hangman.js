ae4e49052402a9bfd66322ce3a3b2aaebfb53969
heidinyt <heidi.erwin@nytimes.com>
2021-06-15 13:23:22 -0400

    hangman desktop-friendly

--- prototypes/hangman/hangman.js
+++ prototypes/hangman/hangman.js
@@ -2,7 +2,7 @@ var answer;
 
 function Hangman () {
     this.possibleWords = ["ZOE", "BEEATRICE", "EVERDEEN", "JONATHAN", "PIZZA"];
-    this.answer = "hi";
+    this.answer = "";
     this.letters = []; // guesses player has already made
     this.incorrectGuessesLeft = 5;
 
